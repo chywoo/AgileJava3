5b0b4cb78ac73a3cbd1c1d39e8954f777fca57b2
choigunho <choiguenho@gmail.com>
2015-09-05 02:02:08 +0900

    CourseSessionTest add

---
+++ AgileJava/src/test/java/CourseSessionTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CourseSessionTest {
+
+	@Test
+	public void testCreate(){
+		CourseSession session = new CourseSession("ENGL", "101");
+		assertEquals("ENGL", session.getDepartment());
+	}
+}
