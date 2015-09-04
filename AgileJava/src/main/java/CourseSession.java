a1f6bea3c51d85665bf3e366e133972e1e303f11
choigunho <choiguenho@gmail.com>
2015-09-05 01:56:03 +0900

    CourseSession class 추

---
+++ AgileJava/src/main/java/CourseSession.java
@@ -0,0 +1,19 @@
+
+public class CourseSession {
+	private String department;
+	private String number;
+	
+	CourseSession(String department, String number){
+		this.department = department;
+		this.number = number;
+	}
+	
+	String getDepartment(){
+		return department;
+	}
+	
+	String getNumber(){
+		return number;
+	}
+
+}
