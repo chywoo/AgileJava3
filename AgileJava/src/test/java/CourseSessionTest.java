2126782d94cbd62cf0f612f578386b30fd41e0d1
choigunho <choiguenho@gmail.com>
2015-09-05 02:11:47 +0900

    수정

--- AgileJava/src/test/java/CourseSessionTest.java
+++ AgileJava/src/test/java/CourseSessionTest.java
@@ -8,5 +8,6 @@ public class CourseSessionTest {
 	public void testCreate(){
 		CourseSession session = new CourseSession("ENGL", "101");
 		assertEquals("ENGL", session.getDepartment());
+		assertEquals("101", session.getNumber());
 	}
 }
